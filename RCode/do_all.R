2ab9719b7291303b4c31355dc20b164f6edc46ee
Kelly McCaskey <kellymccaskey@gmail.com>
2014-08-13 10:32:10 -0400

    fix filepaths

--- RCode/do_all.R
+++ RCode/do_all.R
@@ -20,32 +20,32 @@ d <- read.dta("Data/apsr_replication.dta")
 
 # migration x DTT on international portfolio investment 
 # (the left side of Table 2)
-source("R Code/ipf_taxation_interaction.R")
+source("RCode/ipf_taxation_interaction.R")
 
 # migration x DTT on FDI
 # (the right side of Table 2)
-source("R Code/fdi_taxation_interaction.R")
+source("RCode/fdi_taxation_interaction.R")
 
 # marginal effects plot 
 # (Table 1, Figure 1)
-source("R Code/plot_marginal_effects.R")
+source("RCode/plot_marginal_effects.R")
 
 # international portfolio investment outliers & leverage points
 # (Table 3, Figure 2)
-source("R Code/ipf_outliers.R")
+source("RCode/ipf_outliers.R")
 
 # FDI outliers & leverage points
 # (Figure 3)
-source("R Code/fdi_outliers.R")
+source("RCode/fdi_outliers.R")
 
 # international portfolio investment robust estimate coefficient plot
 # (Figure 4)
 # (this is a robustness check so it is not necessary to run but be aware it will
 # take 30+ minutes to run)
-# source("R Code/robust_ipf.R")
+# source("RCode/robust_ipf.R")
 
 # FDI robust estimate coefficient plot
 # (Figure 5)
 # (this is a robustness check so it is not necessary to run but be aware it will
 # take 30+ minutes to run)
-# source("R Code/robust_fdi.R")
+source("RCode/robust_fdi.R")
